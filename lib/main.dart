7ca37f5398eae3970a2b84ecdc3e017513bf0208
Evan Ferrara <la208881@student.helha.be>
2022-12-28 18:04:17 +0100

    Design + code for authentification

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:music_app/firebase_options.dart';
 import 'package:music_app/pages/connexion_page.dart';
-import 'package:music_app/pages/search_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/pages/connexion_page.dart
+++ lib/pages/connexion_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:music_app/pages/search_page.dart';
-import '../firebase/firebase.dart';
+import '../firebase/firebase.dart'; 
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ConnexionPage extends StatefulWidget {
   const ConnexionPage({super.key, required this.title});
@@ -11,7 +12,14 @@ class ConnexionPage extends StatefulWidget {
 }
 
 class _ConnexionPageState extends State<ConnexionPage> {
-FireBaseService fireBaseService = FireBaseService();
+  FireBaseService fireBaseService = FireBaseService();
+
+  bool isCreating = false;
+  bool isLoggin = false;
+  bool success = false;
+  late String userEmail;
+  late String userPassword;
+  late String userConfirmPassword;
 
   @override
   Widget build(BuildContext context) {
@@ -20,50 +28,124 @@ FireBaseService fireBaseService = FireBaseService();
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-              Container(
-                padding:const EdgeInsets.only(bottom: 20),
-                child: const Text(
-                  "Enter your pseudo",
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-                ),
-              ),
-            SizedBox(
-              height:   100,
-              width: 500,
-              child: Container(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Pseudo',
-                  ),
-                  onSubmitted: (String value) {
-                    if(value != ""){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SearchMusicView(value)),
-                      );
-                    }
-                    else{
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Pseudo non valide'),
-                        ),
-                      );
-                    }
-                  }
-                ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: const Text(
+              "Enter your e-mail and password",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
             ),
+          ),
+          SizedBox(
+            height: 100,
+            width: 500,
+            child: Container(
+              padding: const EdgeInsets.only(
+                  left: 20, right: 20, bottom: 20, top: 20),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'e-mail',
+                ),
+                onChanged: (value) => userEmail = value,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 100,
+            width: 500,
+            child: Container(
+              padding: const EdgeInsets.only(
+                  left: 20, right: 20, bottom: 5, top: 20),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+                onChanged: (value) => userPassword = value,
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(
+            left: 20, right: 20, bottom: 20),
+            child: isCreating ? TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Confirmate Password',
+              ),
+              onChanged: (value) => userConfirmPassword = value,
+            ):
+            const SizedBox(height: 0, width: 0),
+          ),
+          Container(
+            padding: const EdgeInsets.only(bottom: 10), 
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  isCreating = true;
+                  isLoggin = false;
+                });
+              },
+              child: isCreating ? const SizedBox(height: 0, width: 0)
+               :const Text(
+                "Create an account",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 15,
+                  decoration: TextDecoration.underline,
+                ),
+              )
+              ),
+          ),
+            InkWell(
+            onTap: () {
+              setState(() {
+                isCreating = false;
+                isLoggin = true;
+              });
+            },
+            child: isLoggin ? const SizedBox(height: 0, width: 0)
+             :const Text(
+              "Login",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 15,
+                decoration: TextDecoration.underline,
+              ),
+            )
+            ),
+            isCreating ? ElevatedButton(onPressed: createAccount, child: const Text("Create an account")) : 
+            ElevatedButton(onPressed: loginToFirebase, child: const Text("Login")),
         ]),
       ),
     );
+  }
+
+  Future<void> loginToFirebase() async {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: userEmail, password: userPassword);
+      goToSearchView();
+  }
+
+  Future<void> createAccount() async {
+    if (userPassword == userConfirmPassword) {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: userEmail, password: userPassword);
+    }
+    else{
+      const snackBar = SnackBar(content: Text('Passwords are not the same'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+  
+  void goToSearchView() {
+    Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SearchMusicView("rt")));
   }
 }
